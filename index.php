d34aecaa965aff1278767f3ea3e10ead12244a9c
sabbir hossain <mdsabbir477470@gmail.com>
2023-08-03 02:41:50 +0600

    home work done.

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php 
+    // My Home Work one.
+    echo "<h1>1. echo and print difference in php.</h1>";
+    echo "<h3>Echo example</h3>";
+    echo "<li>Echo can pass multiple string separated as (,).</li> <br>";
+    echo "<li>Echo dosen't return any value.</li> <br>";
+    echo "<li>Echo is faster than print.</li> <br>";
+    echo "<li>In PHP, echo is not function but a language construct.</li> <br>";
+
+    echo "<h3>Print example</h3>";
+    echo "<li>Using print dosen't multiple argument.</li> <br>";
+    echo "<li>print Always return value 1</li> <br>";
+    echo "<li>it is slower then echo.</li> <br>";
+    echo "<li>print is not a really function but a language construct. However, it behaves like a function it that it returns a value.</li> <br>";
+
+    // My Home Work Two.
+    echo "<h1>2. echo use kore nijer somporke 15 ta from field.</h1>";
+
+    echo "<h5>First Name: Sabbir</h5>";
+    echo "<h5>Last Name: Hossain</h5>";
+    echo "<h5>Email address: mdsabbir477470@gmail.com</h5>";
+    echo "<h5>Password Name: bangladesh101</h5>";
+    echo "<h5>Confirm Password: bangladesh101</h5>";
+    echo "<h5>Phone Number: 01888559287</h5>";
+    echo "<h5>Emergency Number: 01888559287</h5>";
+    echo "<h5>Collage: Habiganj Polytechnic Institute.</h5>";
+    echo "<h5>Present Address: Habiganj, sylhet.</h5>";
+    echo "<h5>Permanent Address: Rangpur, Bangladesh.</h5>";
+    echo "<h5>Zip code: 5430</h5>";
+    echo "<h5>Website Link: https://dev-sabbir.web.app/</h5>";
+    echo "<h5>Github Link: https://github.com/sabbirvai</h5>";
+    echo "<h5>Linkedin link: https://www.linkedin.com/in/md-sabbir-hossain-18300b244/</h5>";
+    echo "<h5>Facebook Link: https://www.facebook.com/profile.php?id=100066520511940</h5>";
+
+    // My home work done !
+?>
